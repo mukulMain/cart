4a7233584dce19d8e582868b07d4ed170bce9714
mukul soni <mukulsoni2593@gmail.com>
2022-12-02 14:07:15 -0500

    change3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,16 @@
 import React from "react";
-// import { useState } from "react";
 import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import { Link } from 'react-router-dom';
 import Badge from "@mui/material/Badge";
 import { NavLink } from "react-router-dom";
-// import Menu from "@mui/material/Menu";
-// import MenuItem from "@mui/material/MenuItem";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 import { useSelector } from "react-redux";
 
 
 const Header = () => {
-  const getData = useSelector((state) => state.cartReducer)
+  const getData = useSelector((state) => state.cartReducer);
   console.log(getData);
   return (
     <>
@@ -27,25 +24,49 @@ const Header = () => {
               Home
             </NavLink>
           </Nav>
-          <Badge badgeContent={4} color="primary">
-            {/* <i
-              className="fa-solid fa-cart-shopping text-light"
-              style={{ fontSize: 25, cursor: "pointer" }}
-            ></i> */}
-            <Dropdown>
-      <Dropdown.Toggle variant="dark" >
-      <i
-              className="fa-solid fa-cart-shopping text-light"
-              style={{ fontSize: 25, cursor: "pointer" }}
-            ></i>
-      </Dropdown.Toggle>
+          <Badge badgeContent={getData.carts.length} color="primary">
 
-      <Dropdown.Menu>
-        {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item> */}
-        <Dropdown.Item href="#/action-3">your cart is empty</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+            <Dropdown>
+              <Dropdown.Toggle variant="dark">
+                <i
+                  className="fa-solid fa-cart-shopping text-light"
+                  style={{ fontSize: 25, cursor: "pointer" }}
+                ></i>
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                  <Table>
+                    <thead>
+                      <tr>
+                        <th>
+                          Image
+                        </th>
+                        <th>
+                          Restaurant
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {/* <tr>
+                        <td>
+                          <img alt="" fluid="true" src={e.imgdata}/>
+                        </td>
+                      </tr> */}
+                      {/* {getData .map((e)=>{
+                        return(
+                            <>
+                              <tr>
+                                <td>
+                                  hjjgjgj
+                                </td>
+                              </tr>
+                            </>
+                        )
+                      })} */}
+                    </tbody>
+                  </Table>
+              </Dropdown.Menu>
+            </Dropdown>
           </Badge>
         </Container>
       </Navbar>
@@ -54,10 +75,6 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
 
 // function BasicExample() {
 //   return (
@@ -76,3 +93,25 @@ export default Header;
 // }
 
 // export default BasicExample;
+
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+// function AutoLayoutExample () {
+//   return (
+//     <Container>
+//       <Row>
+//         <Col>1 of 2</Col>
+//         <Col>2 of 2</Col>
+//       </Row>
+//       <Row>
+//         <Col>1 of 3</Col>
+//         <Col>2 of 3</Col>
+//         <Col>3 of 3</Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default AutoLayoutExample;
